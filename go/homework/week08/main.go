0a72e4ddbfaa1d2ef9d9b756a93c2d58903c01a1
yujian <15986799513@163.com>
2021-07-19 18:36:24 +0800

    1

--- go/homework/week08/main.go
+++ go/homework/week08/main.go
@@ -29,14 +29,6 @@ func main()  {
 	write(10000, "len10_10k", generateValue(10))
 	write(50000, "len10_50k", generateValue(10))
 	write(500000, "len10_500k", generateValue(10))
-
-	write(10000, "len1000_10k", generateValue(1000))
-	write(50000, "len1000_50k", generateValue(1000))
-	write(500000, "len1000_500k", generateValue(1000))
-
-	write(10000, "len5000_10k", generateValue(5000))
-	write(50000, "len5000_50k", generateValue(5000))
-	write(500000, "len5000_500k", generateValue(5000))
 	analysis()
 	
 }
