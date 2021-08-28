e3c73df3a2eb57e28f12a6bd7389224b29aeaa6c
deluyi <deluyi@i9i8.com>
2021-08-28 19:01:55 +0800

    cron

--- cron.go
+++ cron.go
@@ -10,20 +10,20 @@ import (
 func main(){
 	log.Println("Starting...")
 	c := cron.New()
-	c.AddFunc("* * * * * * *", func() {
-		log.Println("Run models.ClearAllTag...")
+	c.AddFunc("* * * * * *", func() {
+		log.Println("Run models.CleanAllTag...")
 		models.CleanAllTag()
 	})
-	c.AddFunc("* * * * * * *", func() {
-		log.Println("Run models.ClearAllArticle...")
+	c.AddFunc("* * * * * *", func() {
+		log.Println("Run models.CleanAllArticle...")
 		models.CleanAllArticle()
 	})
 	c.Start()
-	t1 := time.NewTimer(time.Second*10)
+	t1 := time.NewTimer(time.Second * 10)
 	for  {
 		select {
 		case <-t1.C:
-			t1.Reset(time.Second*10)
+			t1.Reset(time.Second * 10)
 		}
 	}
 }
